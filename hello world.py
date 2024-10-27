3afe77bd9a639515029f8ef61beefeba3ada6165
Rhys398 <rhyslaird27@gmail.com>
2024-10-27 14:13:07 +0000

    Update hello world.py
    This is an updated version (v1.0.2)

--- hello world.py
+++ hello world.py
@@ -1,1 +1,7 @@
-print("hello world") 
+hello = input("enter the word hello:")
+world = input("enter the word world
+
+while hello != "hello" or hello != "Hello":
+    print ("ERROR user didnt enter hello")
+    hello = input("enter the word hello:")
+
